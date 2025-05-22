ac5737798e794273a7eb0ffec2bb76777200ed4f
Dylan <dylan.calle@ucb.edu.bo>
2025-05-22 09:40:23 -0400

    mas funciones para probar cobertura

--- src/sumador.js
+++ src/sumador.js
@@ -4,4 +4,10 @@ function sumar(a, b) {
 function restar(a, b) {
   return a - b;
 }
-export default sumar;
+function multiplicar(a, b) {
+  return a * b;
+}
+function dividir(a, b) {
+  return a / b;
+}
+export {sumar, restar, multiplicar, dividir};
