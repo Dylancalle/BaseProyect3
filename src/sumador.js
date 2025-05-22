169531ce411f0624c482a1f1fc72ccb81d7d7bb5
Dylan <dylan.calle@ucb.edu.bo>
2025-05-22 08:33:38 -0400

    Se agrego un quinto commit para probar cobertura al cincuenta por ciento

--- src/sumador.js
+++ src/sumador.js
@@ -1,4 +1,7 @@
 function sumar(a, b) {
+  if (a < 0 || b < 0) {
+    return 0; // lógica adicional no cubierta en el test
+  }
   return a + b;
 }
 
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -4,6 +4,9 @@ describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
+  it("debería sumar dos números positivos", () => {
+    expect(sumar(3, 2)).toEqual(5);
+  });
 });
 
 
