2008893c89c189ab555d0809c13541bbff6250c7
Dylan <dylan.calle@ucb.edu.bo>
2025-05-22 08:13:15 -0400

    Se agregaron dos archivos con pruebas y codigo de negocio basicos, primer paso del tdd

---
+++ src/Esbisiesto.js
@@ -0,0 +1,12 @@
+function Esbisiesto(numero) {
+    let resul = false
+    if(numero % 400 == 0)
+    {
+        resul= true
+    }else{
+        resul= false
+    }
+    return resul
+  }
+  
+  export default Esbisiesto;
